8c45566ba06314f0d5fa68f15ff54c90e32c1570
KoroDev01 <malifaiz03@gmail.com>
2026-06-11 00:51:27 +0200

    s

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -9,17 +9,19 @@ exports.app = app;
 require("./config/session.config");
 require("./config/passport.config");
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://artipro01.fr"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(router);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
